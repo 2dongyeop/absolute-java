52441f72403c748b4880e9d14c8f6d4790e3b142
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-10 13:38:03 +0900

    week9 code upload & .gitignore modify

---
+++ src/assignment/week9/GameLauncher.java
@@ -0,0 +1,9 @@
+package newCode.major.assignment.week9;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame guessGame = new GuessGame();
+
+        guessGame.startGame();
+    }
+}
