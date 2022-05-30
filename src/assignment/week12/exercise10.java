7d138b92d62ffb63e24cd27e275b5750784232ae
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-05-30 14:09:13 +0900

    연습문제 10번 풀이

--- src/assignment/week12/exercise10.java
+++ src/assignment/week12/exercise10.java
@@ -6,20 +6,23 @@ public class exercise10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         MyData md = new MyData("김미현", "010-3675-8754", "SK텔레콤");
-        String name;
 
         System.out.println("검색할 이름을 적으세요. >> ");
-        name = scanner.next();
+        String name = scanner.next();
 
-        System.out.println("입력하신 " + name + "의 전화번호는 " + md.getPhoneNum() + "," +
-                "이동 통신사는 " + md.getTelecom() + "입니다.");
+        if (name == md.getName().toString()) {
+            System.out.println("입력하신 " + name + "의 전화번호는 " + md.getPhoneNum() + "," +
+                    "이동 통신사는 " + md.getTelecom() + "입니다.");
+        } else {
+            System.out.println("해당 이름의 사용자가 없습니다.");
+        }
     }
 }
 
 class MyData<K, V1, V2> {
-    K name;
-    V1 phoneNum;
-    V2 telecom;
+    private K name;
+    private V1 phoneNum;
+    private V2 telecom;
 
     MyData(K name, V1 phoneNum, V2 telecom) {
         this.name = name;
